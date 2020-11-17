dfe67b9dfcb2c360b8f8760da730e99a5a95cde9
francyfox <7info7web@gmail.com>
2020-11-17 08:55:45 +0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    function options() {
+        this.ButtonsWrapper = document.querySelector('.grid-4-5');
+        this.Buttons = this.ButtonsWrapper.children;
+        this.ScreenInput = this.ButtonsWrapper.querySelector('#input');
+        this.ScreenOutput = this.ButtonsWrapper.querySelector('#output');
+    }
+
+    function Device() {  // fabric function
+
+    }
+
+    function ScreenInterface(){  //input, output message
+        return [
+
+        ]
+    }
+
+    function calculate(){  // math options
+
+    }
+
+    function SwitchOperator(){  // change operator
+
+    }
+
+    function SystemOptions(){  // AC, Brackets, point, equal
+
+    }
+
+    (function() {
+
+    })()  // init
+
+
+});
