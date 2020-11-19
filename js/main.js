078f61ffd331ccc0680b33effc53ae7e3d0d1e21
francyfox <7info7web@gmail.com>
2020-11-19 08:58:37 +0600

    may be later?

--- js/main.js
+++ js/main.js
@@ -1,23 +1,62 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-    function options() {
-        this.ButtonsWrapper = document.querySelector('.grid-4-5');
+    let ButtonsObj = {}
+    let numbers = {};
+    let operators = {};
+
+    function options(ButtonsWrapper, ScreenInput, ScreenOutput, SystemOperators) {
+
+        ButtonsWrapper = (ButtonsWrapper) ? ButtonsWrapper: '.grid-4-5';
+        ScreenInput = (ScreenInput) ? ScreenInput: '#input';
+        ScreenOutput = (ScreenOutput) ? ScreenOutput: '#output';
+        SystemOperators = (SystemOperators) ? SystemOperators: ['AC', 'equal', 'point'];
+
+        this.ButtonsWrapper = document.querySelector(ButtonsWrapper);
         this.Buttons = this.ButtonsWrapper.children;
-        this.ScreenInput = this.ButtonsWrapper.querySelector('#input');
-        this.ScreenOutput = this.ButtonsWrapper.querySelector('#output');
+        this.ScreenInput = this.ButtonsWrapper.querySelector(ScreenInput);
+        this.ScreenOutput = this.ButtonsWrapper.querySelector(ScreenOutput);
+        this.SystemOperators = SystemOperators;
+
+        for (var i = 0; i < this.Buttons.length; i++){
+            let getId = this.Buttons[i].getAttribute('id');
+            let But = this.Buttons[i];
+
+            if(isNaN(getId)){
+                operators[getId] = But;
+                ButtonsObj['operators'] = operators;
+                But.addEventListener("click", touchOp);
+
+            }else {
+                numbers[getId] = But;
+                ButtonsObj['numbers'] = numbers;
+                But.addEventListener("click", touchNum);
+            }
+        }
+
+        return this;
+
     }
 
-    function Device() {  // fabric function
-
+    function SendMsg(msg) {
+        this.SendMsg = SendMsg;
     }
 
-    function ScreenInterface(){  //input, output message
-        return [
-
-        ]
+    function ReadMsg(msg) {
+        this.ReadMsg = ReadMsg;
     }
 
-    function calculate(){  // math options
+    function touchNum() {
+        SendMsg('')
+        console.log(options().ScreenInput);
+    }
+
+    function touchOp() {
+        console.log(this.innerText);
+    }
+
+
+
+    function calculate() {  // math options
 
     }
 
@@ -29,8 +68,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     }
 
-    (function() {
+    function ScreenInterface(input, output){  //input, output message
 
+        return [
+
+        ]
+    }
+
+    function Device() {  // fabric
+        const opt = new options();
+        ScreenInterface(opt.ScreenInput, opt.ScreenOutput);
+
+    }
+
+    (function() {
+        Device();
     })()  // init
 
 
