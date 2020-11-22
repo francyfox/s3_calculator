285e90395117b9ec834b0a911b739c50a0cfd153
francyfox <7info7web@gmail.com>
2020-11-22 18:40:14 +0600

    add string Input text with multiply

--- js/main.js
+++ js/main.js
@@ -13,8 +13,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
         this.ButtonsWrapper = document.querySelector(ButtonsWrapper);
         this.Buttons = this.ButtonsWrapper.children;
-        this.ScreenInput = this.ButtonsWrapper.querySelector(ScreenInput);
-        this.ScreenOutput = this.ButtonsWrapper.querySelector(ScreenOutput);
+        this.ScreenInput = document.querySelector(ScreenInput);
+        this.ScreenOutput = document.querySelector(ScreenOutput);
         this.SystemOperators = SystemOperators;
 
         for (var i = 0; i < this.Buttons.length; i++){
@@ -25,65 +25,102 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 operators[getId] = But;
                 ButtonsObj['operators'] = operators;
                 But.addEventListener("click", touchOp);
+                // document.addEventListener('keydown', event, touchOp);
 
             }else {
                 numbers[getId] = But;
                 ButtonsObj['numbers'] = numbers;
                 But.addEventListener("click", touchNum);
+                // document.addEventListener('keydown', touchNum);
             }
         }
 
         return this;
-
     }
 
-    function SendMsg(msg) {
-        this.SendMsg = SendMsg;
+    var InputArr = [];
+    var NumString = '';
+    var ScreenInterface = {
+
+        InputMode: function (msg, num, hasOperator){
+            console.log(InputArr);
+            if (hasOperator){
+                NumString = '';
+                InputArr[num] = msg;
+                options().ScreenInput.innerText = InputArr.join(' ');
+            }else{
+                NumString += msg;
+                InputArr[num] = NumString;
+                options().ScreenInput.innerText = InputArr.join(' ');
+            }
+        },
+        OutputMode: function (msg){
+            options().ScreenOutput.innerText = InputArr.join(' ');
+            InputArr = [];
+            options().ScreenInput.innerText = '0';
+        },
+        ReadMode: function (){
+            return {
+                in : this.InputMode,
+                out : this.OutputMode
+            }
+        }
     }
 
-    function ReadMsg(msg) {
-        this.ReadMsg = ReadMsg;
-    }
+
+    var DeviceInterface = Object.create(ScreenInterface);
+    var counter = 0;
 
     function touchNum() {
-        SendMsg('')
-        console.log(options().ScreenInput);
+        // console.log(event.key);
+        // // if ((this.key).match(/[0-9%\/*\-+\(\)=]|Backspace|Enter/)){
+        // //
+        // // }
+
+        if (this.innerText == '0' && counter == 0){
+            counter = 0;
+        }else{
+            DeviceInterface.InputMode(this.innerText, counter, false);
+        }
     }
 
     function touchOp() {
-        console.log(this.innerText);
+        switch (this.id) {
+            case options().SystemOperators[0]: // AC
+                InputArr = [];
+                DeviceInterface.InputMode('0', 0);
+                break;
+            case options().SystemOperators[1]: // equal
+                if (InputArr != []){
+                    DeviceInterface.OutputMode(this.innerText);
+                }
+                DeviceInterface.InputMode('', 0);
+                break;
+            case options().SystemOperators[2]:  // point
+                break;
+            case 'plus':
+                counter++;
+                DeviceInterface.InputMode(this.innerText, counter, true);
+                counter++;
+        }
+    }
+    
+    function calc() {
+
     }
 
-
-
-    function calculate() {  // math options
-
+    function Device() {
+        options();
     }
 
-    function SwitchOperator(){  // change operator
-
-    }
-
-    function SystemOptions(){  // AC, Brackets, point, equal
-
-    }
-
-    function ScreenInterface(input, output){  //input, output message
-
-        return [
-
-        ]
-    }
-
-    function Device() {  // fabric
-        const opt = new options();
-        ScreenInterface(opt.ScreenInput, opt.ScreenOutput);
-
-    }
 
     (function() {
         Device();
     })()  // init
 
+    /*
+    1. Записываем в массив входные данные (числа и операторы)
+    2. Поиск операторов слева направо по приоритету (скобки, умнож, деление, плюс, минус) приоритет
+     */
 
 });
